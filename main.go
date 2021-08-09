140c0650d9115f3de27aba1af665b90d2d1bee05
myra1234 <34545961+myra1234@users.noreply.github.com>
2021-08-08 23:16:20 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main (){
+	name:= "Myra"
+	age:= 20
+
+	fmt.Println("Hello, my name is ",name, "and my age is ",age)
+    TodayIsMyBirthday(&age)
+	fmt.Println("After party my age is ",age)
+}
+
+func TodayIsMyBirthday(age *int) int{
+	*age = 21
+
+	fmt.Println("Today is my ",*age," birthday")
+	return *age
+}
